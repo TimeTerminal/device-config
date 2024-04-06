78eb72879e1f141b4ece305d8cc5191912360d83
Vishesh <vishesh.thanki@gmail.com>
2024-04-05 23:13:34 -0400

    Add Mac main script

---
+++ mac/user_folder/scripts/main.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#  ------------------------------------------------------------
+#  Aliases
+#  ------------------------------------------------------------
+alias f='open .'  # Opens current directory in Windows Explorer
+alias ls='ls -la'
